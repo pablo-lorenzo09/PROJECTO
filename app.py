2a3b8cb7ffb334a1d568c7b97567625f883b0633
Pablo Moralez <pablo.moralez@aluno.senai.br>
2026-04-02 14:52:51 -0300

    comittada loca

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, request, redirect, session, flash
+import mysql
+import mysql.connector
+
+app = Flask (__name__)
+
+@app.route("/")
+@app.route("/home",methods=["GET"])
+def pag_principal():
+    return render_template("index.html")
+
+if __name__=="__main__":
+    app.run(host="0.0.0.0",port=8080,debug=True)
