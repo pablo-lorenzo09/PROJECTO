3c391dfd32a14bbdce5e6bb8dfb200bff6e67ebd
Pablo Moralez <pablo.moralez@aluno.senai.br>
2026-04-23 16:51:52 -0300

    dasbjsaljd

--- static/src/js/carrinho.js
+++ static/src/js/carrinho.js
@@ -11,16 +11,16 @@ async function mostrar_carrinho(){
 
         for (let dado of dados){
             let linha = `    
-            
+        <div class="cart-item" >    
             <img src="${dado.imagem}" alt="Hambúrguer" class="cart-item__image">
         <div class="cart-item__info">
             <div class="cart-item__title">${dado.nome}</div>
             <div class="cart-item__price">${dado.preco}</div>
             <div class="cart-item__description">Pão, hambúrguer 180g, queijo, bacon, alface, tomate e molho especial.</div>
         </div>
-        
+        </div>
         `
-        carrinho.innerHTML = linha;
+        carrinho.innerHTML += linha;
         };
     
     };
