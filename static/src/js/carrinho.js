1aed1643d75af710fcf24fcc72c6a62134fc2bd3
Pablo Moralez <pablo.moralez@aluno.senai.br>
2026-05-08 10:43:13 -0300

    masi besteira

--- static/src/js/carrinho.js
+++ static/src/js/carrinho.js
@@ -21,7 +21,13 @@ async function mostrar_carrinho(){
             <div class="cart-item__price">${dado.preco}.00</div>
             <div class="cart-item__description">Pão, hambúrguer 180g, queijo, bacon, alface, tomate e molho especial.</div>
         </div>
+            <button class="cart-item__delete">
+                <svg class="delete-icon" viewBox="0 0 24 24" width="20" height="20">
+                <path fill="currentColor" d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z" />
+                </svg>
+            </button>
         </div>
+
         `
         carrinho.innerHTML += linha;
         // cartBadge.innerText += 1
